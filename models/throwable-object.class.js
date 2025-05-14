388b99ae092c6bee7a206d4bce4433f38c6ef8c8
EmreIscan-47 <iscan.connect@outlook.de>
2025-05-14 12:55:34 +0200

    Splash Bottles added

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,14 +1,40 @@
 class ThrowableObjects extends MovableObject {
 speedX;
 speedY;
+IMAGES_BOTTLES = [
+    "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png"
 
+];
+
+IMAGES_SPLASH = [
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png"
+    
+];
 constructor (x, y) {
     super().loadImage("img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png");
+    this.loadImages(this.IMAGES_BOTTLES);
+    this.loadImages(this.IMAGES_SPLASH);
+    this.throwBottleAnimation();
     this.x = 0;
     this.y = 0;
     this.height = 100;
     this.width = 80;
     this.throw(x, y);
+
+}
+
+throwBottleAnimation(){
+    setInterval(() => {
+            this.playAnimation(this.IMAGES_BOTTLES)
+    }, 100);
 }
 
     throw(x, y) {
@@ -17,7 +43,14 @@ constructor (x, y) {
       this.speedY = 20;
       this.applyGravity();
       setInterval(() => {
-        this.x += 10;
+        if (this.y > 296) {
+
+            this.applyGravity();
+            this.playAnimation(this.IMAGES_SPLASH)
+        
+        } else {
+            this.x += 10;
+        }
       }, 25)
     }
 }
