90df544391a6006e1863075db36397f4da62dc5b
EmreIscan-47 <iscan.connect@outlook.de>
2025-05-26 21:10:04 +0200

    Little Chicken added

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -13,7 +13,7 @@ class Chicken extends MovableObject {
     
     constructor () {
         super();
-        this.name = "chicken;"
+        this.name = "chicken";
         this.loadImage('img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
         this.x = 200 + Math.random() * 1500;
         this.y = 340;
---
+++ models/little-chicken.class.js
@@ -0,0 +1,41 @@
+class LittleChicken extends MovableObject {
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+  ];
+  constructor() {
+    super();
+        this.name = "little_chicken";
+        this.loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        this.x = 200 + Math.random() * 1500;
+        this.y = 370;
+        this.height = 50;
+        this.width = 50;
+        this.loadImages(this.IMAGES_WALKING);
+        this.animateLittleChicken();
+        this.speed = 1.5 + Math.random() * 1.25;
+  }
+
+  chickenDead() {
+      const deadIntervall = setInterval(() => {
+         clearInterval(this.animateChickenInterval);
+      clearInterval(this.animateLeftInterval);
+      this.loadImage("img/3_enemies_chicken/chicken_small/2_dead/dead.png");
+      setInterval(() => {
+        this.loadImage("");
+        clearInterval(deadIntervall);
+        this.y = 1000;
+      }, 500);
+      }, 200);
+     
+    }
+  animateLittleChicken() {
+       this.animateChickenInterval = setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING)
+          }, 6500 / 60);
+        /*  this.animateLeftInterval = setInterval(() => {
+            this.moveLeft();
+          }, 2000 / 60); */
+    }
+}
